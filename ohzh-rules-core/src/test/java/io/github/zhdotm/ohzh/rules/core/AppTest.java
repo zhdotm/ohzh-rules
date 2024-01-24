2d1fc502052c890186b56d16770716026eb6d06c
ANTHEM37 <85991202+zhdotm@users.noreply.github.com>
2024-01-25 05:14:05 +0800

    test: 测试复合规则

--- ohzh-rules-core/src/test/java/io/github/zhdotm/ohzh/rules/core/AppTest.java
+++ ohzh-rules-core/src/test/java/io/github/zhdotm/ohzh/rules/core/AppTest.java
@@ -8,8 +8,12 @@ import io.github.zhdotm.ohzh.rules.core.domain.action.impl.SingleAction;
 import io.github.zhdotm.ohzh.rules.core.domain.condition.ICondition;
 import io.github.zhdotm.ohzh.rules.core.domain.condition.impl.*;
 import io.github.zhdotm.ohzh.rules.core.domain.rule.IRule;
+import io.github.zhdotm.ohzh.rules.core.domain.rule.impl.AllEvaluatePassAllExecuteCompositeRule;
+import io.github.zhdotm.ohzh.rules.core.domain.rule.impl.AnyEvaluatePassAllExecuteCompositeRule;
 import io.github.zhdotm.ohzh.rules.core.domain.rule.impl.SingleRule;
+import io.github.zhdotm.ohzh.rules.core.domain.rule.impl.WhichEvaluatePassWhichExecuteCompositeRule;
 import org.jeasy.rules.api.Facts;
+import org.jeasy.rules.api.Rule;
 import org.jeasy.rules.api.Rules;
 import org.jeasy.rules.core.DefaultRulesEngine;
 
@@ -17,7 +21,7 @@ import java.util.Map;
 
 public class AppTest {
 
-    public static void main(String[] args) {
+    public static void main1(String[] args) {
         DefaultRulesEngine defaultRulesEngine = new DefaultRulesEngine();
         Rules rules = new Rules();
         //条件 (inventoryQuantity > 100 && inventoryQuantity < 500) || (companyType == travelCompany)
@@ -52,5 +56,55 @@ public class AppTest {
         //执行结果
         Map<String, Object> executeReturnMap = rule1.getExecuteReturnMap();
         System.out.println(JSONUtil.toJsonPrettyStr(executeReturnMap));
+    }
+
+    public static void main(String[] args) {
+        IRule rule1 = new SingleRule("rule1", "规则1", new GreatThanCondition<>("fieldName001", 100), new SingleAction<Integer>("doNothingFieldName001", "doNothing", facts -> facts.get("fieldName001")));
+        rule1.setPriority(1);
+        IRule rule2 = new SingleRule("rule2", "规则2", new GreatThanCondition<>("fieldName001", 200), new SingleAction<>("doubleFieldName001", "doubleAction", facts -> ((Integer) facts.get("fieldName001")) * 2));
+        rule2.setPriority(2);
+        IRule rule3 = new SingleRule("rule3", "规则3", new GreatThanCondition<>("fieldName001", 300), new SingleAction<>("tripleFieldName001", "tripleAction", facts -> ((Integer) facts.get("fieldName001")) * 3));
+        rule2.setPriority(3);
+
+        AnyEvaluatePassAllExecuteCompositeRule anyEvaluatePassAllExecuteCompositeRule = new AnyEvaluatePassAllExecuteCompositeRule(rule1, rule2, rule3);
+
+        DefaultRulesEngine defaultRulesEngine = new DefaultRulesEngine();
+
+        Rules rules = new Rules(rule1, rule2, rule3);
+        Facts facts = new Facts();
+        facts.put("fieldName001", 101);
+        Map<Rule, Boolean> check = defaultRulesEngine.check(rules, facts);
+        check.forEach((rule, aBoolean) -> {
+            System.out.println(rule.getName() + ": " + aBoolean);
+        });
+
+//        defaultRulesEngine.fire(rules, facts);
+//        System.out.println(JSONUtil.toJsonStr(rule1.getExecuteReturnMap()));
+//        System.out.println(JSONUtil.toJsonStr(rule2.getExecuteReturnMap()));
+//        System.out.println(JSONUtil.toJsonStr(rule3.getExecuteReturnMap()));
+
+        //一个条件匹配，所有动作执行
+        rules.clear();
+        rules.register(anyEvaluatePassAllExecuteCompositeRule);
+        defaultRulesEngine.fire(rules, facts);
+        System.out.println(JSONUtil.toJsonStr(anyEvaluatePassAllExecuteCompositeRule.getExecuteReturnMap()));
+
+        //哪个条件匹配，哪个动作执行
+        WhichEvaluatePassWhichExecuteCompositeRule whichEvaluatePassWhichExecuteCompositeRule = new WhichEvaluatePassWhichExecuteCompositeRule(rule1, rule2, rule3);
+        rules.clear();
+        rules.register(whichEvaluatePassWhichExecuteCompositeRule);
+        defaultRulesEngine.fire(rules, facts);
+        System.out.println(JSONUtil.toJsonStr(whichEvaluatePassWhichExecuteCompositeRule.getExecuteReturnMap()));
+
+        //所有条件匹配，所有动作执行
+        AllEvaluatePassAllExecuteCompositeRule allEvaluatePassAllExecuteCompositeRule = new AllEvaluatePassAllExecuteCompositeRule(rule1, rule2, rule3);
+        rules.clear();
+        rules.register(allEvaluatePassAllExecuteCompositeRule);
+        defaultRulesEngine.fire(rules, facts);
+        System.out.println(JSONUtil.toJsonStr(allEvaluatePassAllExecuteCompositeRule.getExecuteReturnMap()));
+        facts.put("fieldName001", 1000);
+        defaultRulesEngine.fire(rules, facts);
+        System.out.println(JSONUtil.toJsonStr(allEvaluatePassAllExecuteCompositeRule.getExecuteReturnMap()));
+
     }
 }
